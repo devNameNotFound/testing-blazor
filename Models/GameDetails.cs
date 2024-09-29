eafad6989a004ccf312ebaff756a98056c60328f
devNameNotFound <devsamaritan@gmail.com>
2024-09-29 13:17:16 +0200

    added a new page editgame and form component

---
+++ Models/GameDetails.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace GameStore.frontend.Models;
+
+public class GameDetails
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? GenreId {get; set;}
+    
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+    
+}
+
